159ca75ae23a9a652b9cbbf09b6c6ac81398b2fa
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-06-29 09:05:51 +0700

    docs: add detail comment for cmd_utils and feat get current path

--- cmd/cmd_utils/utils.go
+++ cmd/cmd_utils/utils.go
@@ -6,7 +6,8 @@ import (
 	"path/filepath"
 )
 
-// Lấy đường dẫn hiện tại của dự án đã tạo của người dùng
+// En: Get the current path of the user's created project.
+// Vi: Lấy đường dẫn hiện tại của dự án đã tạo của người dùng
 func GetCurrentPath() string {
 	path, err := os.Getwd()
 	if err != nil {
@@ -15,7 +16,8 @@ func GetCurrentPath() string {
 	return path
 }
 
-// Lấy đường dẫn của migrate/migrations/ của bên người dùng
+// En: Get the path to migrate/migrations/ from the user side.
+// Vi: Lấy đường dẫn của migrate/migrations/ của bên người dùng
 func GetMigrationsPath() string {
 	pwd, err := os.Getwd()
 	if err != nil {
@@ -24,8 +26,8 @@ func GetMigrationsPath() string {
 	return filepath.Join(pwd, "migrate", "migrations")
 }
 
-// Lấy đường dẫn đến framework (nơi chứa binary witches.exe)
-// Lấy thông tin và đường dẫn đến tài nguyên của framework để tái sử dụng
+// En: Get the path to the framework (where the binary witches.exe is located)
+// Vi: Lấy đường dẫn đến framework (nơi chứa binary witches.exe)
 func GetFrameworkPath() string {
 	exe, err := os.Executable()
 	if err != nil {
