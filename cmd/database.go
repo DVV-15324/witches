ce28ed1906cd527350dc25324d5ad203fb2c36ec
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-06-29 10:39:55 +0700

    docs: add detail comment for database.go

--- cmd/database.go
+++ cmd/database.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	//"fmt"
-
 	cmd_database "github.com/DVV-15324/witches/cmd/cmd_database"
 	godotenv "github.com/joho/godotenv"
 	"github.com/spf13/cobra"
@@ -10,36 +8,43 @@ import (
 	"os"
 )
 
+// En: Database command group
+// Vi: Nhóm lệnh quản lý database
+
 var databaseCmd = &cobra.Command{
 	Use:   "database",
-	Short: "Quản lý database",
-	Long: `Container database:
-			- postgres
-			- mySQL
-			- mSSQL
-			- redis`,
+	Short: "En: Database management Vi: Quản lý database",
+	Long: `Database containers:
+			- PostgreSQL
+			- MySQL
+			- MSSQL
+			Defalt:
+			- Redis`,
 	Run: func(cmd *cobra.Command, args []string) {
+		// En: Load environment variables from witches.env
+		// Vi: Kiểm tra biến môi trường từ file witches.env
 		err := godotenv.Load("witches.env")
 		if err != nil {
-			log.Fatal("Error loading .env file")
+			log.Fatal("Error: loading .env file")
 		}
 	},
 }
 
 var databaseDockerUpCmd = &cobra.Command{
 	Use:   "docker-up",
-	Short: "Tao database",
-	Long: `Tạo container database:
+	Short: "Deploy database containers Vi: Triển khai database containers",
+	Long: `Database containers:
 			- PostgreSQL
 			- MySQL
 			- MSSQL
 			Defalt:
-			- Migrate
 			- Redis`,
 	Run: func(cmd *cobra.Command, args []string) {
+		// En: Load environment variables from witches.env
+		// Vi: Kiểm tra biến môi trường từ file witches.env
 		err := godotenv.Load("witches.env")
 		if err != nil {
-			log.Fatal("Error loading .env file")
+			log.Fatal("Error: loading .env file")
 		}
 
 		cmd_database.WitchesDatabaseDockerUp(os.Getenv("DB_DRIVER"))
@@ -48,19 +53,21 @@ var databaseDockerUpCmd = &cobra.Command{
 
 var databaseDockerDownCmd = &cobra.Command{
 	Use:   "docker-down",
-	Short: "Tao database",
-	Long: `Tạo container database:
+	Short: "Down database containers Vi: Xóa database containers",
+	Long: `Database containers:
 			- PostgreSQL
 			- MySQL
 			- MSSQL
 			Defalt:
-			- Migrate
 			- Redis`,
 	Run: func(cmd *cobra.Command, args []string) {
+		// En: Load environment variables from witches.env
+		// Vi: Kiểm tra biến môi trường từ file witches.env
 		err := godotenv.Load("witches.env")
 		if err != nil {
-			log.Fatal("Error loading .env file")
+			log.Fatal("Error: loading .env file")
 		}
+
 		cmd_database.WitchesDatabaseDockerDown()
 	},
 }
