08cf0688151cabd519dde6f1ae3c259c51e5be0a
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-07-12 08:37:12 +0700

    chore: update cmd/cmd_run/create.go

--- cmd/cmd_run/create.go
+++ cmd/cmd_run/create.go
@@ -1,15 +1,16 @@
 package cmd
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+
+	content "github.com/DVV-15324/witches/cmd/cmd_utils"
 )
 
 // En: This function creates a new project with environment configuration
 // Vi: Hàm tạo dự án mới với file cấu hình môi trường
-func WitchesCreate(project string, DB_DRIVER string) {
+func WitchesCreate(project string, projectType string, DB_DRIVER string) {
 	projectPath := filepath.Join(".", project)
 	err := os.MkdirAll(projectPath, os.ModePerm)
 	if err != nil {
@@ -30,16 +31,18 @@ func WitchesCreate(project string, DB_DRIVER string) {
 	//En: Make sure the file is closed
 	//Vi: Chắc chắn file đóng
 	defer file.Close()
-	_, err = file.WriteString(
-		"APP_PORT=YOUR_PORT_APP\n" +
-			"DB_PASSWORD=YOUR_PASSWORD\n" +
-			"DB_NAME=YOUR_DATABASE\n" +
-			"DB_HOST=DB_HOST\n" +
-			"DB_PORT=YOUR_PORT_APP\n" +
-			"REDIS_PORT=REDIS_PORT\n" +
-			fmt.Sprintf("DB_DRIVER=%s\n", DB_DRIVER),
-	)
-	if err != nil {
-		log.Fatal(err)
+	if projectType == "refresh" {
+		content := content.CreateContentRefresh(DB_DRIVER)
+		_, err = file.WriteString(content)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		content := content.CreateContentAccess(DB_DRIVER)
+		_, err = file.WriteString(content)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+
 }
