6afd32ad5423a25ade2c3b0e1ef557cfddda7ade
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-07-30 04:41:53 +0700

    chore: update cmd\migrate\migrate_up.go

--- cmd/migrate/migrate_up.go
+++ cmd/migrate/migrate_up.go
@@ -1,28 +1,31 @@
 package cmd_migrate
 
 import (
-	utils "github.com/DVV-15324/witches/cmd/cmd_utils"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+
+	utils "github.com/DVV-15324/witches/cmd/utils"
 )
 
-// En: Apply 1 pending funciton
-// Vi: Chức năng triển khai Migrate
-func WitchesMigrateUp(DB_URL string) {
-	//En: Get the path to the migrate/migrations/ folder
-	//Vi: Lấy đường dẫn đến folder migrate/migrations/
-	migratePath := utils.GetMigrationsPath()
+// En: Apply 1 pending function with database driver support
+// Vi: Chức năng triển khai Migrate với hỗ trợ nhiều loại database
+func WitchesMigrateUp(DB_URL string, DB_DRIVER string) {
+	// En: Get the path to the migrate/migrations/ folder
+	// Vi: Lấy đường dẫn đến folder migrate/migrations/
+	migratePath := utils.GetMigrationsURL()
 
-	//En: Start executing
-	//Vi: Bắt đầu thực thi
+	// En: Build full database URL based on driver
+	// Vi: Xây dựng URL đầy đủ dựa trên driver
+	fullDBURL := utils.BuildDatabaseURL(DB_DRIVER, DB_URL)
+	fmt.Println(migratePath, fullDBURL)
+	// En: Start executing using local migrate binary
+	// Vi: Bắt đầu thực thi sử dụng binary migrate local
 	cmd := exec.Command(
-		"docker", "run", "--rm",
-		"-v", migratePath+":/migrations",
-		"--network", "host",
-		"migrate/migrate",
-		"-path=/migrations",
-		"-database", DB_URL,
+		"migrate", // Sử dụng migrate binary đã cài đặt trên hệ thống
+		"-path", migratePath,
+		"-database", fullDBURL,
 		"up", "1",
 	)
 	cmd.Stdout = os.Stdout
