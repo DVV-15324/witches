0b4273cab9f72be18eec5eb582313093fdd2641f
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-07-30 04:42:05 +0700

    chore: update cmd\migrate\migrate_version.go

--- cmd/migrate/migrate_version.go
+++ cmd/migrate/migrate_version.go
@@ -1,27 +1,31 @@
 package cmd_migrate
 
 import (
-	utils "github.com/DVV-15324/witches/cmd/cmd_utils"
+	utils "github.com/DVV-15324/witches/cmd/utils"
 	"log"
 	"os"
 	"os/exec"
 )
 
-// En: This function displays the current Migrate version.
-// Vi: Hàm hiển thị version Migrate hiện tại
-func WitchesMigrateVersion(DB_URL string) {
-	//En: Get the path to the migrate/migrations/ folder
-	//Vi: Lấy đường dẫn đến folder migrate/migrations/
-	migratePath := utils.GetMigrationsPath()
-	//En: Start executing
-	//Vi: Bắt đầu thực thi
-	cmd := exec.Command("docker", "run", "--rm",
-		"-v", migratePath+":/migrations",
-		"--network", "host",
-		"migrate/migrate",
-		"-path=/migrations",
-		"-database", DB_URL,
-		"version")
+// En: Show current migration version
+// Vi: Hiển thị phiên bản migration hiện tại
+func WitchesMigrateVersion(DB_URL string, DB_DRIVER string) {
+	// En: Get the path to the migrate/migrations/ folder
+	// Vi: Lấy đường dẫn đến folder migrate/migrations/
+	migratePath := utils.GetMigrationsURL()
+
+	// En: Build full database URL based on driver
+	// Vi: Xây dựng URL đầy đủ dựa trên driver
+	fullDBURL := utils.BuildDatabaseURL(DB_DRIVER, DB_URL)
+
+	// En: Start executing using local migrate binary
+	// Vi: Bắt đầu thực thi sử dụng binary migrate local
+	cmd := exec.Command(
+		"migrate", // Sử dụng migrate binary đã cài đặt trên hệ thống
+		"-path", migratePath,
+		"-database", fullDBURL,
+		"version",
+	)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
