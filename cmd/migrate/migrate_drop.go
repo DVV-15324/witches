06603376b92e7d5d98da074ac0d1ae2edb19742d
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-07-30 04:41:21 +0700

    chore: update cmd\migrate\migrate_drop.go

--- cmd/migrate/migrate_drop.go
+++ cmd/migrate/migrate_drop.go
@@ -1,29 +1,27 @@
 package cmd_migrate
 
 import (
-	utils "github.com/DVV-15324/witches/cmd/cmd_utils"
+	utils "github.com/DVV-15324/witches/cmd/utils"
 	"log"
 	"os"
 	"os/exec"
 )
 
-// En: This function will clear the database
-// Vi: Chức năng này sẽ xóa sạch database
-func WitchesMigrateDrop(DB_URL string) {
-	//En: Get the path to the migrate/migrations/ folder
-	//Vi: Lấy đường dẫn đến folder migrate/migrations/
-	migratePath := utils.GetMigrationsPath()
-	//En: Start executing
-	//Vi: Bắt đầu thực thi
-	cmd := exec.Command("docker", "run", "--rm",
-		"-v", migratePath+":/migrations",
-		"--network", "host",
-		"migrate/migrate",
-		"-path=/migrations",
-		"-database", DB_URL,
-		"drop", "-f")
+// En: Drop all tables in database (force)
+// Vi: Xóa tất cả bảng trong database (force)
+func WitchesMigrateDrop(DB_URL string, DB_DRIVER string) {
+	migratePath := utils.GetMigrationsURL()
+	fullDBURL := utils.BuildDatabaseURL(DB_DRIVER, DB_URL)
+
+	cmd := exec.Command(
+		"migrate",
+		"-path", migratePath,
+		"-database", fullDBURL,
+		"drop", "-f",
+	)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
