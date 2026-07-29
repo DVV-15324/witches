9c08cbc0ccb9e28397213f6db1a90d035e6e6b66
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-07-30 04:40:59 +0700

    chore: update cmd\migrate\migrate_down.go

--- cmd/migrate/migrate_down.go
+++ cmd/migrate/migrate_down.go
@@ -1,31 +1,27 @@
 package cmd_migrate
 
 import (
-	utils "github.com/DVV-15324/witches/cmd/cmd_utils"
+	utils "github.com/DVV-15324/witches/cmd/utils"
 	"log"
 	"os"
 	"os/exec"
 )
 
-// En: Rollback Migrate function
-// Vi: Chức năng rollback Migrate
-func WitchesMigrateDown(DB_URL string) {
-	//En: Get the path to the migrate/migrations/ folder.
-	//Vi: Lấy đường dẫn đến folder migrate/migrations/
-	migratePath := utils.GetMigrationsPath()
+// / En: Rollback migration down
+// Vi: Rollback migration xuống
+func WitchesMigrateDown(DB_URL string, DB_DRIVER string) {
+	migratePath := utils.GetMigrationsURL()
+	fullDBURL := utils.BuildDatabaseURL(DB_DRIVER, DB_URL)
 
-	//En: Start executing
-	//Vi: Bắt đầu thực thi
-	cmd := exec.Command("docker", "run", "--rm",
-		"-v", migratePath+":/migrations",
-		"--network", "host",
-		"migrate/migrate",
-		"-path=/migrations",
-		"-database", DB_URL,
-		"down", "1")
-
+	cmd := exec.Command(
+		"migrate",
+		"-path", migratePath,
+		"-database", fullDBURL,
+		"down", "1",
+	)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
