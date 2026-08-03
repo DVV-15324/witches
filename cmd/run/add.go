2ae77685740e3b1b5ef0027bb868a33fbd0bfdbe
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-03 11:07:23 +0700

    feat: update cmd\run\add.go

---
+++ cmd/run/add.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"os"
+	"path/filepath"
+
+	templates "github.com/DVV-15324/witches/pkg/core/templates"
+)
+
+// En: This function creates template for project
+// Vi : Hàm tạo templates cho dự án
+func WitchesAdd(serviceN string) {
+	projectPath, _ := os.Getwd()
+	//Vi: Kiểm tra thư mục hiện tại và tên dự án
+	//En: Check the current directory and project name
+	moduleName := filepath.Base(projectPath)
+	// n: Execute template
+	//Vi: Thực thi template
+
+	templates.AddGoService(projectPath, moduleName, serviceN)
+}
