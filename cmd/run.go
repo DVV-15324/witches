4136d4b5f502067dc0af3919ac7e5793bebf1cd5
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-03 11:08:13 +0700

    chore: update cmd\run.go

--- cmd/run.go
+++ cmd/run.go
@@ -62,10 +62,34 @@ var installCmd = &cobra.Command{
 	Long:  "En: This function installs dependencies Vi: Hàm cài đặt các thư viện cần dùng",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) > 0 {
-			fmt.Println("Error: missing run project")
+			fmt.Println("Error: missing install project")
 			return
 		}
 		db_driver := os.Getenv("DB_DRIVER")
 		run.WitchesInstall(db_driver)
+	},
+}
+
+var addCmd = &cobra.Command{
+	Use:   "add <service_name>",
+	Short: "En: Add template for project Vi: Hàm thêm templates cho dự án",
+	Long:  "En: Add template for project Vi: Hàm thêm templates cho dự án",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Error: missing service name")
+			fmt.Println("Usage: witches add <service_name>")
+			fmt.Println("Example: witches add book")
+			return
+		}
+		serviceName := args[0]
+
+		// En: Load environment variables from witches.env
+		// Vi: Kiểm tra biến môi trường từ file witches.env
+		err := godotenv.Load("witches.env")
+		if err != nil {
+			log.Fatal("Error: missing load")
+		}
+
+		run.WitchesAdd(serviceName)
 	},
 }
