e4730c704e2a84e84dc5190cca61a46d25af01a4
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-05 21:46:51 +0700

    chore: update pkg\core\templates\utils\utils.go

---
+++ pkg/core/templates/utils/utils.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"embed"
+	"html/template"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type Config interface {
+	GetMuduleName() string
+}
+
+func RenderTemplate(templateFS embed.FS, baseDir, destFile, tmplFile string, config Config) {
+	// Đọc trực tiếp từ embed.FS
+	tmpl, err := template.ParseFS(templateFS, tmplFile)
+	if err != nil {
+		log.Fatalf("Error: parse template %s: %v", tmplFile, err)
+	}
+
+	fullPath := filepath.Join(baseDir, destFile)
+	if err := os.MkdirAll(filepath.Dir(fullPath), 0755); err != nil {
+		log.Fatalf("Error: create folder for %s: %v", fullPath, err)
+	}
+
+	file, err := os.Create(fullPath)
+	if err != nil {
+		log.Fatalf("Error: create file %s: %v", fullPath, err)
+	}
+	defer file.Close()
+
+	// Execute template trực tiếp
+	if err := tmpl.Execute(file, config); err != nil {
+		log.Fatalf("Error: execute template %s: %v", tmplFile, err)
+	}
+}
