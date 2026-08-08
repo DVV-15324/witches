73918f94ee618dfe6ba5b80a419cb0429f46fa61
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-08 18:40:34 +0700

    chore: update pkg\core\utils\metrict.go

--- pkg/core/utils/metrict.go
+++ pkg/core/utils/metrict.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -14,22 +15,23 @@ import (
 
 // InitMetric khởi tạo middleware metrics và chạy metrics server trên một goroutine riêng.
 // Trả về gin.Engine đã được bọc middleware .
-func InitMetric(metricsAddr string, engine *gin.Engine) *gin.Engine {
-	if !strings.Contains(metricsAddr, ":") && !strings.HasPrefix(metricsAddr, ":") {
-		metricsAddr = ":" + metricsAddr
+func InitMetric(port string, host string, engine *gin.Engine) *gin.Engine {
+	if !strings.Contains(port, ":") && !strings.HasPrefix(port, ":") {
+		port = ":" + port
 	}
 	// 1. Tạo middleware metrics
 	mdlw := middleware.New(middleware.Config{
 		Recorder: metrics.NewRecorder(metrics.Config{}),
 	})
+	addr := fmt.Sprintf("%s%s", host, port)
 
 	// 2. Gắn middleware vào Gin engine
 	engine.Use(ginmiddleware.Handler("", mdlw))
 
 	// 3. Chạy metrics server trên goroutine riêng
 	go func() {
-		log.Printf("Metrics server listening on %s", metricsAddr)
-		if err := http.ListenAndServe(metricsAddr, promhttp.Handler()); err != nil {
+		log.Printf("Metrics server listening on http://%s", addr)
+		if err := http.ListenAndServe(port, promhttp.Handler()); err != nil {
 			log.Printf("Metrics server error: %v", err)
 		}
 	}()
