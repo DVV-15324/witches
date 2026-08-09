d9add1c687a66d0d60655437c79e01c7bf14380d
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-09 12:55:39 +0700

    chore: cmd\database.go

--- cmd/database.go
+++ cmd/database.go
@@ -1,31 +1,25 @@
 package cmd
 
 import (
-	"log"
-	"os"
-
 	cmd_database "github.com/DVV-15324/witches/cmd/database"
-	godotenv "github.com/joho/godotenv"
+	utils "github.com/DVV-15324/witches/cmd/utils"
 	"github.com/spf13/cobra"
 )
 
 var databaseCmd = &cobra.Command{
-	Use:   "database",
-	Short: "Deploy database containers Vi: Triển khai database containers",
-	Long: `Database containers:
-			- PostgreSQL
-			- MySQL
-			- MSSQL
-			Defalt:
-			- Redis`,
+	Use: "database",
 	Run: func(cmd *cobra.Command, args []string) {
-		// En: Load environment variables from witches.env
-		// Vi: Kiểm tra biến môi trường từ file witches.env
-		err := godotenv.Load("witches.env")
-		if err != nil {
-			log.Fatal("Error: loading .env file")
-		}
+		cfg := utils.PreloadDBURL()
 
-		cmd_database.WitchesDatabase(os.Getenv("DB_DRIVER"))
+		cmd_database.WitchesDBURL(cfg.DBDriver, cfg)
+	},
+}
+
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "Generate DB_URL from witches.env",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := utils.PreloadDBURL()
+		cmd_database.WitchesDBURL(cfg.DBDriver, cfg)
 	},
 }
