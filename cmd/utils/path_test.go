6aa9b9fb210d6fbba61cb5f5e009b41308d90f8e
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-10 07:00:05 +0700

    feat: add cmd\utils\path_test.go

---
+++ cmd/utils/path_test.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"os"
+
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetCurrentPath(t *testing.T) {
+	path := GetCurrentPath()
+	assert.NotEmpty(t, path)
+
+	// Check if path exists
+	_, err := os.Stat(path)
+	assert.NoError(t, err)
+}
+
+func TestGetMigrationsPath(t *testing.T) {
+	path := GetMigrationsPath()
+	assert.NotEmpty(t, path)
+	assert.Contains(t, path, "migrate")
+	assert.Contains(t, path, "migrations")
+}
+
+func TestGetMigrationsURL(t *testing.T) {
+	url := GetMigrationsURL()
+	assert.NotEmpty(t, url)
+	assert.Contains(t, url, "migrate")
+	assert.Contains(t, url, "migrations")
+}
+
+func TestGetFrameworkPath(t *testing.T) {
+	path := GetFrameworkPath()
+	assert.NotEmpty(t, path)
+}
