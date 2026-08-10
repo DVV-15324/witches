b2f8409ae9242ecdd88d1ef91f08ed1a42e60924
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-10 08:49:52 +0700

    chore: update test\handler\rate_limit\handler_test.go

--- test/handler/rate_limit/handler_test.go
+++ test/handler/rate_limit/handler_test.go
@@ -27,6 +27,9 @@ func resetRedis(mr *miniredis.Miniredis) {
 	mr.FlushAll()
 }
 func TestHandleSwaggerRateLimit(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
+	}
 	gin.SetMode(gin.TestMode)
 	r := gin.Default()
 
