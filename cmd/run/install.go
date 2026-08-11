c42790db08aa230a7f28f82ec7c81013742fb16b
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-11 18:43:18 +0700

    chore: update cmd\run\install.go

--- cmd/run/install.go
+++ cmd/run/install.go
@@ -10,23 +10,30 @@ func WitchesInstall(DB_DRIVER string) {
 	tools := []string{
 		"github.com/mailru/easyjson/easyjson@latest",
 		"github.com/golang-migrate/migrate/v4/cmd/migrate@latest",
-		"github.com/mailru/easyjson/jwriter",
-		"github.com/mailru/easyjson/jlexer",
 	}
 	drivers := map[string]string{
 		"mysql":      "github.com/golang-migrate/migrate/v4/database/mysql@latest",
-		"postgres":   "github.comgolang-migrate/migrate/v4/database/postgres@latest",
+		"postgres":   "github.com/golang-migrate/migrate/v4/database/postgres@latest",
 		"postgresql": "github.com/golang-migrate/migrate/v4/database/postgres@latest",
 		"mssql":      "github.com/golang-migrate/migrate/v4/database/sqlserver@latest",
 		"sqlserver":  "github.com/golang-migrate/migrate/v4/database/sqlserver@latest",
 	}
-	runCmd("go", "mod", "tidy")
-	if driverPath, ok := drivers[DB_DRIVER]; ok {
-		runCmd("go", "get", driverPath)
-	}
+
 	for _, tool := range tools {
+		log.Printf("  Installing %s...", tool)
 		runCmd("go", "install", tool)
 	}
+
+	if driverPath, ok := drivers[DB_DRIVER]; ok {
+		log.Printf("  Adding driver: %s", DB_DRIVER)
+		runCmd("go", "get", driverPath)
+	}
+
+	runCmd("go", "get", "github.com/mailru/easyjson@latest")
+
+	runCmd("go", "mod", "tidy")
+
+	log.Println("Installation complete!")
 }
 
 func runCmd(name string, args ...string) {
