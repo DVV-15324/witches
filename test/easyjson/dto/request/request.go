e8b05a516c20bf83e71528213c761a4aa870c699
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-12 11:54:15 +0700

    feat: remove test\easyjson

--- test/easyjson/dto/request/request.go
+++
@@ -1,7 +0,0 @@
-package request
-
-type CreateUser struct {
-	Name string
-}
-
-func (u *CreateUser) GenEasyJson() {}
--- test/easyjson/dto/response/response.go
+++
@@ -1,8 +0,0 @@
-package response
-
-type User struct {
-	ID   string
-	Name string
-}
-
-func (u *User) GenEasyJson() {}
