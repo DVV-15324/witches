758447c0b1c432199a7f0b4a538cc8f83faa510d
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-16 03:17:44 +0700

    chore: update pkg\core\templates\template.go[skip ci]

--- pkg/core/templates/template.go
+++ pkg/core/templates/template.go
@@ -12,7 +12,7 @@ import (
 //go:embed template/cmd/*.tmpl
 //go:embed template/cmd/server/config/*.tmpl
 //go:embed template/cmd/server/routers/*.tmpl
-//go:embed template/cmd/core/core.go.tmpl
+//go:embed template/cmd/server/core/core.go.tmpl
 //go:embed template/internal/auth/dto/request/*.tmpl
 //go:embed template/internal/auth/dto/response/*.tmpl
 //go:embed template/internal/auth/model/*.tmpl
