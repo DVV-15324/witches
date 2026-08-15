870cedd80b0e02f8655f1d9a65f045dbc394aacf
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-15 08:54:57 +0700

    chore: update pkg\core\utils\cors.go[skip ci]

---
+++ pkg/core/utils/cors.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"github.com/DVV-15324/witches/cmd/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Cors(cfg *utils.Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
+
+		// Lấy từ config
+		allowOrigin := cfg.CorsAllowOrigins
+		if allowOrigin == "" {
+			allowOrigin = "*"
+		}
+		c.Writer.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+
+		allowMethods := cfg.CorsAllowMethods
+		if allowMethods == "" {
+			allowMethods = "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+		}
+		c.Writer.Header().Set("Access-Control-Allow-Methods", allowMethods)
+
+		allowHeaders := cfg.CorsAllowHeaders
+		if allowHeaders == "" {
+			allowHeaders = "Content-Type, Authorization"
+		}
+		c.Writer.Header().Set("Access-Control-Allow-Headers", allowHeaders)
+
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
