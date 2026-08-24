49b6ddc8ccbe2472b3349afaa5ec33f1c97674f9
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-24 11:40:32 +0700

    chore: update pkg\core\utils\shutdown_grateful.go[skip ci]

--- pkg/core/utils/shutdown_grateful.go
+++ pkg/core/utils/shutdown_grateful.go
@@ -10,38 +10,28 @@ import (
 	"time"
 )
 
-func ShutdownServer(ctx context.Context, engine http.Handler, address string, port string) {
-	// Tạo addr = address:port
-	addr := fmt.Sprintf("%s:%s", address, port)
-
-	// Create HTTP server
-	srv := &http.Server{
+func ShutdownServer(ctx context.Context, engine http.Handler, host string, port string) {
+	addr := fmt.Sprintf("%s:%s", host, port)
+	server := &http.Server{
 		Addr:    addr,
 		Handler: engine,
 	}
-
-	// Run server in goroutine
 	go func() {
 		log.Printf("Server running on http://%s\n", addr)
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
-
-	// Wait for interrupt signal
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
 
 	log.Println("Shutdown Server ...")
-	// Graceful shutdown with timeout
-	_ = ctx
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	if err := srv.Shutdown(ctx); err != nil {
+	if err := server.Shutdown(ctx); err != nil {
 		log.Fatal("Server forced to shutdown:", err)
 	}
-
 	log.Println("Server exiting")
 }
