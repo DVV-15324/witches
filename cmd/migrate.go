d5a2d53a01cad9b7b2553ea0227a77e2642dcb26
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-24 11:47:44 +0700

    chore: update cmd\migrate.go[skip ci]

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	cmd_migrate "github.com/DVV-15324/witches/cmd/migrate"
 	utils "github.com/DVV-15324/witches/cmd/utils"
 	"github.com/spf13/cobra"
@@ -23,31 +25,63 @@ var migrateDropCmd = &cobra.Command{
 
 var migrateUpCmd = &cobra.Command{
 	Use: "up",
-	Run: func(cmd *cobra.Command, args []string) {
+
+	RunE: func(cmd *cobra.Command, args []string) error {
 		cfg := utils.PreloadNotDBURL()
 		utils.LoadDbUrl(cfg)
+
+		switch cfg.Env {
+		case "development", "test":
+		case "production":
+			return fmt.Errorf(
+				"migrate command is not allowed in production",
+			)
+		default:
+			return fmt.Errorf("invalid ENV: %s", cfg.Env)
+		}
 		migratePath := utils.GetMigrationsURL()
 		cmd_migrate.WitchesMigrateUp(cfg.DBUrl, cfg.DBDriver, migratePath)
+		return nil
 	},
 }
 
 var migrateDownCmd = &cobra.Command{
 	Use: "down",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		cfg := utils.PreloadNotDBURL()
 		utils.LoadDbUrl(cfg)
+		switch cfg.Env {
+		case "development", "test":
+		case "production":
+			return fmt.Errorf(
+				"migrate command is not allowed in production",
+			)
+		default:
+			return fmt.Errorf("invalid ENV: %s", cfg.Env)
+		}
 		migratePath := utils.GetMigrationsURL()
 		cmd_migrate.WitchesMigrateDown(cfg.DBUrl, cfg.DBDriver, migratePath)
+		return nil
 	},
 }
 
 var migrateVersionCmd = &cobra.Command{
 	Use: "force",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		cfg := utils.PreloadNotDBURL()
 		utils.LoadDbUrl(cfg)
+		switch cfg.Env {
+		case "development", "test":
+		case "production":
+			return fmt.Errorf(
+				"migrate command is not allowed in production",
+			)
+		default:
+			return fmt.Errorf("invalid ENV: %s", cfg.Env)
+		}
 		migratePath := utils.GetMigrationsURL()
 		cmd_migrate.WitchesMigrateForce(cfg.DBUrl, cfg.DBDriver, migratePath, args[0])
+		return nil
 	},
 }
 var migrateForceCmd = &cobra.Command{
