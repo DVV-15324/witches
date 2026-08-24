a27652959b0d9ef6631ff4abd532bfce2633e960
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-24 11:39:31 +0700

    chore: update pkg\core\utils\metrict.go[skip ci]

--- pkg/core/utils/metrict.go
+++ pkg/core/utils/metrict.go
@@ -13,22 +13,15 @@ import (
 	ginmiddleware "github.com/slok/go-http-metrics/middleware/gin"
 )
 
-// InitMetric khởi tạo middleware metrics và chạy metrics server trên một goroutine riêng.
-// Trả về gin.Engine đã được bọc middleware .
-func InitMetric(port string, host string, engine *gin.Engine) *gin.Engine {
+func InstanceMetric(port string, host string, engine *gin.Engine) *gin.Engine {
 	if !strings.Contains(port, ":") && !strings.HasPrefix(port, ":") {
 		port = ":" + port
 	}
-	// 1. Tạo middleware metrics
 	mdlw := middleware.New(middleware.Config{
 		Recorder: metrics.NewRecorder(metrics.Config{}),
 	})
 	addr := fmt.Sprintf("%s%s", host, port)
-
-	// 2. Gắn middleware vào Gin engine
 	engine.Use(ginmiddleware.Handler("", mdlw))
-
-	// 3. Chạy metrics server trên goroutine riêng
 	go func() {
 		log.Printf("Metrics server listening on http://%s", addr)
 		if err := http.ListenAndServe(port, promhttp.Handler()); err != nil {
