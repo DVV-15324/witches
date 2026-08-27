b3d01393460623a2f33071dafacc9278bdb34210
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-27 12:37:15 +0700

    feat: add pkg\core\templates\utils\utils.go[skip ci]

--- pkg/core/templates/utils/utils.go
+++ pkg/core/templates/utils/utils.go
@@ -13,27 +13,23 @@ type Config interface {
 }
 
 func RenderTemplate(templateFS embed.FS, baseDir, destFile, tmplFile string, config Config) {
-	// Đọc trực tiếp từ embed.FS
 	tmpl, err := template.ParseFS(templateFS, tmplFile)
 	if err != nil {
-		log.Fatalf("Error: parse template %s: %v", tmplFile, err)
+		log.Fatalf("parse template %s: %v", tmplFile, err)
 	}
-
 	fullPath := filepath.Join(baseDir, destFile)
 	if err := os.MkdirAll(filepath.Dir(fullPath), 0755); err != nil {
-		log.Fatalf("Error: create folder for %s: %v", fullPath, err)
+		log.Fatalf("create folder for %s: %v", fullPath, err)
 	}
 
 	file, err := os.Create(fullPath)
 	if err != nil {
-		log.Fatalf("Error: create file %s: %v", fullPath, err)
+		log.Fatalf("create file %s: %v", fullPath, err)
 	}
 	defer func() {
 		_ = file.Close()
 	}()
-
-	// Execute template trực tiếp
 	if err := tmpl.Execute(file, config); err != nil {
-		log.Fatalf("Error: execute template %s: %v", tmplFile, err)
+		log.Fatalf("execute template %s: %v", tmplFile, err)
 	}
 }
