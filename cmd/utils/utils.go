62640fddee870a5e4baf0bcfc5b6a79d3e054ef2
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-31 08:31:00 +0700

    chore: update cmd\utils\utils.go

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func GetCurrentPath() string {
@@ -30,9 +31,27 @@ func GetFrameworkPath() string {
 	return filepath.Dir(exe)
 }
 
-func GetMigrationsURL() string {
+func GetMigrationsURL(pathM string) string {
 	pwd, _ := os.Getwd()
-	// ToSlash => file: ///
-	path := filepath.ToSlash(filepath.Join(pwd, "migrate", "migrations"))
-	return path
+
+	// Xử lý các trường hợp
+	switch {
+	case pathM == "":
+		// Mặc định: migrate/migrations
+		path := filepath.Join(pwd, "migrate", "migrations")
+		os.MkdirAll(path, 0755)
+		return filepath.ToSlash(path)
+
+	case !strings.ContainsAny(pathM, "/\\."):
+		// Domain: internal/{domain}/migrate/migrations
+		path := filepath.Join(pwd, "internal", pathM, "migrate", "migrations")
+		os.MkdirAll(path, 0755)
+		return filepath.ToSlash(path)
+
+	default:
+		// Đường dẫn tùy chỉnh
+		path := filepath.Join(pwd, pathM)
+		os.MkdirAll(path, 0755)
+		return filepath.ToSlash(path)
+	}
 }
