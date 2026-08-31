060e19b1bd736357d2a5578ecfca1e4955a3fea3
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-31 08:26:49 +0700

    chore: update cmd\run\run_test.go[skip ci]

--- cmd/run/run_test.go
+++ cmd/run/run_test.go
@@ -125,59 +125,130 @@ func TestWitchesCreate_EnvFileExists(t *testing.T) {
 	}
 }
 
-func TestWitchesInit_Success(t *testing.T) {
+func TestWitchesLink_Success(t *testing.T) {
+	// Tạo temp directory
 	tmpDir := t.TempDir()
-
-	goModPath := filepath.Join(tmpDir, "go.mod")
-	err := os.WriteFile(goModPath, []byte("module test-module"), 0644)
-	require.NoError(t, err)
-
 	originalWd, err := os.Getwd()
 	require.NoError(t, err)
-	defer func() {
-		if err := os.Chdir(originalWd); err != nil {
-			log.Printf("failed to chdir: %v", err)
-		}
-	}()
-	if err := os.Chdir(tmpDir); err != nil {
-		t.Fatalf("failed to chdir: %v", err)
-	}
+	defer os.Chdir(originalWd)
+
+	// Chuyển vào temp dir
+	err = os.Chdir(tmpDir)
 	require.NoError(t, err)
 
-	WitchesInit("postgres")
+	// Tạo go.mod
+	goModContent := `module captain
 
-	expectedDirs := []string{
-		"cmd",
-		"internal",
-		"pkg",
-		"migrate/migrations",
+go 1.21
+`
+	err = os.WriteFile("go.mod", []byte(goModContent), 0644)
+	require.NoError(t, err)
+
+	// Tạo thư mục cần thiết
+	dirs := []string{
+		"cmd/server/routers",
+		"internal/shared/domain",
+		"internal/shared/middleware",
+		"internal/shared/utils",
 	}
-	for _, dir := range expectedDirs {
-		_, err := os.Stat(dir)
-		assert.NoError(t, err, "Directory %s should be created", dir)
+	for _, dir := range dirs {
+		err = os.MkdirAll(dir, 0755)
+		require.NoError(t, err)
 	}
 
-	_, err = os.Stat("main.go")
-	assert.NoError(t, err, "main.go should exist at root")
+	// Tạo file modules.go
+	modulesContent := `package routers
+
+type Modules struct {
 }
 
-func TestWitchesInit_InvalidDriver(t *testing.T) {
-	if os.Getenv("TEST_SUBPROCESS_INIT") == "1" {
-		WitchesInit("invalid-driver")
-		return
+func (m *Modules) Init() *Modules {
+	return &Modules{}
+}
+`
+	err = os.WriteFile("cmd/server/routers/modules.go", []byte(modulesContent), 0644)
+	require.NoError(t, err)
+
+	// Tạo file routers.go
+	routersContent := `package routers
+
+func SetupRoutes() error {
+	return nil
+}
+`
+	err = os.WriteFile("cmd/server/routers/routers.go", []byte(routersContent), 0644)
+	require.NoError(t, err)
+
+	// Tạo file key_object.go
+	keyObjectContent := `package domain
+
+type KeyObject string
+
+const (
+	User KeyObject = "user"
+)
+`
+	err = os.WriteFile("internal/shared/domain/key_object.go", []byte(keyObjectContent), 0644)
+	require.NoError(t, err)
+
+	// Chạy command
+	WitchesLink("book", "https://github.com/DVV-15324/witches-book.git")
+
+	// Kiểm tra file đã được tạo
+	expectedFiles := []string{
+		"internal/book/module.go",
+		"internal/book/model/model.go",
+		"internal/book/handler/handler.go",
+		"internal/book/repository/repository.go",
+		"internal/book/usecase/usecase.go",
+		"internal/book/migrate/migrations/1_create_table.up.sql",
+		"internal/book/migrate/migrations/1_drop_table.down.sql",
+		"internal/shared/domain/book.go",
 	}
 
-	cmd := exec.Command(os.Args[0], "-test.run=TestWitchesInit_InvalidDriver")
-	cmd.Env = append(os.Environ(), "TEST_SUBPROCESS_INIT=1")
-
-	err := cmd.Run()
-	assert.Error(t, err, "Should exit with error")
-	exitErr, ok := err.(*exec.ExitError)
-	if ok {
-		assert.Equal(t, 1, exitErr.ExitCode(), "Should exit with code 1")
-	} else {
-		t.Errorf("Expected ExitError, got %T", err)
+	for _, file := range expectedFiles {
+		_, err := os.Stat(file)
+		assert.NoError(t, err, "File should exist: %s", file)
 	}
+}
+
+func TestWitchesLink_NoGoMod(t *testing.T) {
+	tmpDir := t.TempDir()
+	originalWd, err := os.Getwd()
+	require.NoError(t, err)
+	defer os.Chdir(originalWd)
+
+	err = os.Chdir(tmpDir)
+	require.NoError(t, err)
+
+	// Không có go.mod, command sẽ fail
+	// Cần capture output hoặc check error
+	WitchesLink("book", "https://github.com/DVV-15324/witches-book.git")
+
+	// Kiểm tra không có file nào được tạo
+	_, err = os.Stat("internal/book")
+	assert.Error(t, err, "Book directory should not be created")
+}
+
+func TestWitchesLink_InvalidRepo(t *testing.T) {
+	tmpDir := t.TempDir()
+	originalWd, err := os.Getwd()
+	require.NoError(t, err)
+	defer os.Chdir(originalWd)
+
+	err = os.Chdir(tmpDir)
+	require.NoError(t, err)
+
+	// Tạo go.mod
+	err = os.WriteFile("go.mod", []byte("module test"), 0644)
+	require.NoError(t, err)
+
+	// Test với repo không tồn tại
+	WitchesLink("book", "https://github.com/invalid/repo.git")
+
+	// Kiểm tra không có file nào được tạo
+	_, err = os.Stat("internal/book")
+	assert.Error(t, err, "Book directory should not be created")
 }
 
 func TestWitchesInstall_Success(t *testing.T) {
@@ -405,74 +476,186 @@ func TestRemoveEasyJSONFiles(t *testing.T) {
 	_, err = os.Stat(normalFile)
 	assert.NoError(t, err, "Normal file should remain")
 }
-func TestWitchesAdd_Success(t *testing.T) {
+func TestWitchesRollback_Success(t *testing.T) {
 	tmpDir := t.TempDir()
-	goModPath := filepath.Join(tmpDir, "go.mod")
-	err := os.WriteFile(goModPath, []byte("module test-module"), 0644)
+	originalWd, err := os.Getwd()
+	require.NoError(t, err)
+	defer os.Chdir(originalWd)
+
+	err = os.Chdir(tmpDir)
 	require.NoError(t, err)
 
-	sharedDir := filepath.Join(tmpDir, "internal", "shared", "utils")
-	err = os.MkdirAll(sharedDir, 0755)
+	// Tạo go.mod
+	err = os.WriteFile("go.mod", []byte("module captain"), 0644)
 	require.NoError(t, err)
-	keyContent := `package utils
 
-var Object uint = 1
+	// Tạo thư mục internal/shared/domain trước
+	err = os.MkdirAll(filepath.Join("internal", "shared", "domain"), 0755)
+	require.NoError(t, err)
+
+	// Tạo domain book
+	bookDir := filepath.Join("internal", "book")
+	err = os.MkdirAll(bookDir, 0755)
+	require.NoError(t, err)
+
+	// Tạo file module.go trong book
+	moduleContent := `package book
+
+type Module struct{}
 `
-	err = os.WriteFile(filepath.Join(sharedDir, "key_object.go"), []byte(keyContent), 0644)
+	err = os.WriteFile(filepath.Join(bookDir, "module.go"), []byte(moduleContent), 0644)
 	require.NoError(t, err)
 
-	originalWd, _ := os.Getwd()
-	defer func() {
-		if err := os.Chdir(originalWd); err != nil {
-			log.Printf("failed to chdir: %v", err)
-		}
-	}()
-	if err := os.Chdir(tmpDir); err != nil {
-		t.Fatalf("failed to chdir: %v", err)
-	}
-
-	WitchesAdd("book")
-
-	serviceDir := filepath.Join(tmpDir, "internal", "book")
-	_, err = os.Stat(serviceDir)
-	assert.NoError(t, err, "Service directory should be created")
-}
-
-func TestWitchesRoll_Success(t *testing.T) {
-	tmpDir := t.TempDir()
-	goModPath := filepath.Join(tmpDir, "go.mod")
-	err := os.WriteFile(goModPath, []byte("module test-module"), 0644)
+	// Tạo shared domain book
+	sharedFile := filepath.Join("internal", "shared", "domain", "book.go")
+	err = os.WriteFile(sharedFile, []byte("package domain"), 0644)
 	require.NoError(t, err)
 
-	sharedDir := filepath.Join(tmpDir, "internal", "shared", "utils")
-	err = os.MkdirAll(sharedDir, 0755)
-	require.NoError(t, err)
-	keyContent := `package utils
-
-var Object uint = 1
-`
-	err = os.WriteFile(filepath.Join(sharedDir, "key_object.go"), []byte(keyContent), 0644)
-	require.NoError(t, err)
-
-	originalWd, _ := os.Getwd()
-	defer func() {
-		if err := os.Chdir(originalWd); err != nil {
-			log.Printf("failed to chdir: %v", err)
-		}
-	}()
-	if err := os.Chdir(tmpDir); err != nil {
-		t.Fatalf("failed to chdir: %v", err)
-	}
-
-	WitchesAdd("book")
-
-	serviceDir := filepath.Join(tmpDir, "internal", "book")
-
-	_, err = os.Stat(serviceDir)
-	require.NoError(t, err, "Service directory should be created")
-
+	// Chạy rollback
 	WitchesRollback("book")
 
-	_, err = os.Stat(serviceDir)
-	assert.Error(t, err, "Service directory should be removed")
+	// Kiểm tra đã xóa
+	_, err = os.Stat(bookDir)
+	assert.Error(t, err, "Book directory should be removed")
+
+	// Kiểm tra shared domain file đã xóa
+	_, err = os.Stat(sharedFile)
+	assert.Error(t, err, "Shared domain file should be removed")
+}
+func TestWitchesAdd_Success(t *testing.T) {
+	tmpDir := t.TempDir()
+	originalWd, err := os.Getwd()
+	require.NoError(t, err)
+	defer os.Chdir(originalWd)
+
+	err = os.Chdir(tmpDir)
+	require.NoError(t, err)
+
+	// Tạo go.mod
+	err = os.WriteFile("go.mod", []byte("module captain"), 0644)
+	require.NoError(t, err)
+
+	// Tạo các thư mục cần thiết
+	dirs := []string{
+		"internal/shared/domain",
+		"internal/shared/utils", // Thêm thư mục utils
+		"internal/shared/middleware",
+		"cmd/server/routers",
+	}
+	for _, dir := range dirs {
+		err = os.MkdirAll(dir, 0755)
+		require.NoError(t, err)
+	}
+
+	// Tạo file key_object.go trong utils
+	keyContent := `package utils
+
+type KeyObject string
+
+const (
+	User KeyObject = "user"
+)
+`
+	err = os.WriteFile("internal/shared/utils/key_object.go", []byte(keyContent), 0644)
+	require.NoError(t, err)
+
+	// Tạo file key_object.go trong domain
+	domainKeyContent := `package domain
+
+type KeyObject string
+
+const (
+	User KeyObject = "user"
+)
+`
+	err = os.WriteFile("internal/shared/domain/key_object.go", []byte(domainKeyContent), 0644)
+	require.NoError(t, err)
+
+	// Tạo file modules.go
+	modulesContent := `package routers
+
+type Modules struct {
+	User *user.Module
+}
+
+func (m *Modules) InitModules() *Modules {
+	return &Modules{
+		User: &user.Module{},
+	}
+}
+`
+	err = os.WriteFile("cmd/server/routers/modules.go", []byte(modulesContent), 0644)
+	require.NoError(t, err)
+
+	// Tạo file routers.go
+	routersContent := `package routers
+
+func SetupRoutes() error {
+	return nil
+}
+`
+	err = os.WriteFile("cmd/server/routers/routers.go", []byte(routersContent), 0644)
+	require.NoError(t, err)
+
+	// Chạy add domain
+	WitchesAdd("book", "postgres")
+
+	// Kiểm tra domain được tạo
+	expectedFiles := []string{
+		"internal/book/module.go",
+		"internal/book/model/model.go",
+		"internal/book/handler/handler.go",
+		"internal/book/repository/repository.go",
+		"internal/book/usecase/usecase.go",
+	}
+
+	for _, file := range expectedFiles {
+		_, err := os.Stat(file)
+		assert.NoError(t, err, "File should exist: %s", file)
+	}
+}
+func TestWitchesRun_WithArgs(t *testing.T) {
+	tmpDir := t.TempDir()
+	originalWd, err := os.Getwd()
+	require.NoError(t, err)
+	defer os.Chdir(originalWd)
+
+	err = os.Chdir(tmpDir)
+	require.NoError(t, err)
+
+	// Tạo go.mod
+	err = os.WriteFile("go.mod", []byte("module test"), 0644)
+	require.NoError(t, err)
+
+	// Tạo main.go
+	mainContent := `package main
+import "fmt"
+func main() {
+	fmt.Println("Hello")
+}
+`
+	err = os.WriteFile("main.go", []byte(mainContent), 0644)
+	require.NoError(t, err)
+
+	// Lưu os.Args cũ và thay đổi
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+
+	os.Args = []string{"witches", "run", "arg1", "arg2"}
+
+	// Chạy với args
+	WitchesRun()
+}
+
+func TestWitchesRun_NoModule(t *testing.T) {
+	tmpDir := t.TempDir()
+	originalWd, err := os.Getwd()
+	require.NoError(t, err)
+	defer os.Chdir(originalWd)
+
+	err = os.Chdir(tmpDir)
+	require.NoError(t, err)
+
+	// Không có go.mod, không có main.go
+	WitchesRun()
 }
