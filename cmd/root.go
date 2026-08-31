8e541aae89dafb8bf3178e4494121379ebec81b3
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-08-31 08:30:10 +0700

    chore: update cmd\root.go[skip ci]

--- cmd/root.go
+++ cmd/root.go
@@ -8,6 +8,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "witches",
 	Version: "v1.1.1",
+	Short:   "Witches CLI - Go project generator",
 	Run: func(cmd *cobra.Command, args []string) {
 		_ = cmd.Help()
 	},
