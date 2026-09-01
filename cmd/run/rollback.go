240d425b7168ad17f4bc50f1da102954d65e35c2
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-09-01 21:08:25 +0700

    chore: update cmd\run\rollback.go[skip ci]

--- cmd/run/rollback.go
+++ cmd/run/rollback.go
@@ -1,18 +1,31 @@
 package cmd
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	templates "github.com/DVV-15324/witches/pkg/core/templates"
 )
 
-func WitchesRollback(domainName string) {
+func WitchesRollback(domainName string) error {
+	if _, err := os.Stat("go.mod"); os.IsNotExist(err) {
+		return fmt.Errorf("go.mod not found")
+	}
+
+	domainPath := filepath.Join("internal", domainName)
+	if _, err := os.Stat(domainPath); os.IsNotExist(err) {
+		return fmt.Errorf("domain '%s' not found", domainName)
+	}
+
 	projectPath, err := os.Getwd()
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		return fmt.Errorf("get working directory: %w", err)
 	}
+
 	moduleName := filepath.Base(projectPath)
-	_ = templates.RollbackDomain(projectPath, moduleName, domainName)
+	if err := templates.RollbackDomain(projectPath, moduleName, domainName); err != nil {
+		return fmt.Errorf("rollback domain: %w", err)
+	}
+	return nil
 }
