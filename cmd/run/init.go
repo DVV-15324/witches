38bfa9b66ffc5c5822faa4c669bb6d5c6bb17c50
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-09-01 21:08:02 +0700

    chore: update cmd\run\init.go[skip ci]

--- cmd/run/init.go
+++ cmd/run/init.go
@@ -1,18 +1,26 @@
 package cmd
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	templates "github.com/DVV-15324/witches/pkg/core/templates"
 )
 
-func WitchesInit(DBdriver string) {
+func WitchesInit(DBdriver string) error {
+	if _, err := os.Stat("witches.env"); os.IsNotExist(err) {
+		return fmt.Errorf("witches.env not found")
+	}
+
 	projectPath, err := os.Getwd()
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		return fmt.Errorf("get working directory: %w", err)
 	}
+
 	moduleName := filepath.Base(projectPath)
-	templates.CreateTemplateGoArc(moduleName, DBdriver)
+	if err := templates.CreateTemplateGoArc(moduleName, DBdriver); err != nil {
+		return fmt.Errorf("create template: %w", err)
+	}
+	return nil
 }
