a7af926613e2c026b739ebcfddc25609ed61cdb4
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-09-01 21:08:13 +0700

    chore: update cmd\run\link.go[skip ci]

--- cmd/run/link.go
+++ cmd/run/link.go
@@ -1,18 +1,26 @@
 package cmd
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	templates "github.com/DVV-15324/witches/pkg/core/templates"
 )
 
-func WitchesLink(domainName, repoURL string) {
+func WitchesLink(domainName, repoURL string) error {
+	if _, err := os.Stat("go.mod"); os.IsNotExist(err) {
+		return fmt.Errorf("go.mod not found")
+	}
+
 	projectPath, err := os.Getwd()
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		return fmt.Errorf("get working directory: %w", err)
 	}
+
 	moduleName := filepath.Base(projectPath)
-	templates.AddGoDomainFromLink(projectPath, moduleName, domainName, repoURL)
+	if err := templates.AddGoDomainFromLink(projectPath, moduleName, domainName, repoURL); err != nil {
+		return fmt.Errorf("link domain: %w", err)
+	}
+	return nil
 }
