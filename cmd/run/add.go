2556d0cacc243e88a04d69b40c3a912d947b8ab9
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-09-01 21:07:39 +0700

    chore: update cmd\run\add.go[skip ci]

--- cmd/run/add.go
+++ cmd/run/add.go
@@ -1,18 +1,31 @@
 package cmd
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	templates "github.com/DVV-15324/witches/pkg/core/templates"
 )
 
-func WitchesAdd(moduleName string, DBdriver string) {
+func WitchesAdd(moduleName string, DBdriver string) error {
+	if _, err := os.Stat("go.mod"); os.IsNotExist(err) {
+		return fmt.Errorf("go.mod not found")
+	}
+
+	domainPath := filepath.Join("internal", moduleName)
+	if _, err := os.Stat(domainPath); err == nil {
+		return fmt.Errorf("domain '%s' already exists", moduleName)
+	}
+
 	projectPath, err := os.Getwd()
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		return fmt.Errorf("get working directory: %w", err)
 	}
+
 	projectName := filepath.Base(projectPath)
-	templates.AddModule(projectPath, projectName, moduleName, DBdriver)
+	if err := templates.AddModule(projectPath, projectName, moduleName, DBdriver); err != nil {
+		return fmt.Errorf("add module: %w", err)
+	}
+	return nil
 }
