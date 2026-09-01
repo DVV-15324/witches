258099cf029c80a6c9af7c19b753be567ffe0489
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-09-01 07:48:40 +0700

    chore: update cmd\run\init.go[skip ci]

--- cmd/run/init.go
+++ cmd/run/init.go
@@ -8,20 +8,11 @@ import (
 	templates "github.com/DVV-15324/witches/pkg/core/templates"
 )
 
-func WitchesInitCaptain(DBdriver string) {
+func WitchesInit(DBdriver string) {
 	projectPath, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
 	moduleName := filepath.Base(projectPath)
-	templates.CreateCaptainGoArc(moduleName, DBdriver)
-}
-
-func WitchesInitMember(DBdriver string) {
-	projectPath, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-	}
-	moduleName := filepath.Base(projectPath)
-	templates.CreateMemberGoArc(moduleName, DBdriver)
+	templates.CreateTemplateGoArc(moduleName, DBdriver)
 }
