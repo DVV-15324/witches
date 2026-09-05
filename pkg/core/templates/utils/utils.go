873d9643b17af22448df6110040cb0677a227a06
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-09-05 10:39:01 +0700

    chore: update pkg\core\templates\utils\utils.go

--- pkg/core/templates/utils/utils.go
+++ pkg/core/templates/utils/utils.go
@@ -8,6 +8,11 @@ import (
 	"text/template"
 )
 
+var (
+	mkdirAll   = os.MkdirAll
+	createFile = os.Create
+)
+
 type Config interface {
 	GetProjectName() string
 }
@@ -17,18 +22,22 @@ func RenderTemplate(templateFS embed.FS, baseDir, destFile, tmplFile string, con
 	if err != nil {
 		log.Fatalf("parse template %s: %v", tmplFile, err)
 	}
+
 	fullPath := filepath.Join(baseDir, destFile)
-	if err := os.MkdirAll(filepath.Dir(fullPath), 0755); err != nil {
+
+	if err := mkdirAll(filepath.Dir(fullPath), 0755); err != nil {
 		log.Fatalf("create folder for %s: %v", fullPath, err)
 	}
 
-	file, err := os.Create(fullPath)
+	file, err := createFile(fullPath)
 	if err != nil {
 		log.Fatalf("create file %s: %v", fullPath, err)
 	}
+
 	defer func() {
 		_ = file.Close()
 	}()
+
 	if err := tmpl.Execute(file, config); err != nil {
 		log.Fatalf("execute template %s: %v", tmplFile, err)
 	}
