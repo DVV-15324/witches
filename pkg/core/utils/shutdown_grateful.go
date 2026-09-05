7b12aa887a1aeb599d1de9dfdc9fa1721a888be0
vudinh11111 <dinhvietvu15032004bn@gmail.com>
2026-09-05 09:06:21 +0700

    chore: update pkg\core\utils\shutdown_grateful.go[skip ci]

--- pkg/core/utils/shutdown_grateful.go
+++ pkg/core/utils/shutdown_grateful.go
@@ -10,28 +10,48 @@ import (
 	"time"
 )
 
+var notifySignal = signal.Notify
+var shutdownServerFunc = func(server *http.Server, ctx context.Context) error {
+	return server.Shutdown(ctx)
+}
+
 func ShutdownServer(ctx context.Context, engine http.Handler, host string, port string) {
 	addr := fmt.Sprintf("%s:%s", host, port)
+
 	server := &http.Server{
 		Addr:    addr,
 		Handler: engine,
 	}
+
 	go func() {
 		log.Printf("Server running on http://%s\n", addr)
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
+
+		if err := server.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("listen: %s\n", err)
 		}
 	}()
+
 	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
+	notifySignal(quit, os.Interrupt)
 	<-quit
 
+	shutdownServer(server)
+}
+
+func shutdownServer(server *http.Server) {
 	log.Println("Shutdown Server ...")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
 	defer cancel()
 
 	if err := server.Shutdown(ctx); err != nil {
-		log.Fatal("Server forced to shutdown:", err)
+		log.Printf("Server forced to shutdown: %v", err)
+		return
 	}
+
 	log.Println("Server exiting")
 }
